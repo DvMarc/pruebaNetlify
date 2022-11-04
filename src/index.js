8bfd05bf15b026d000ec5d6b9adaf755b68c595f
DvMarc <romdmarc@espol.edu.ec>
2022-11-04 17:25:28 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './app-root';
+const express = require('express');
+const app = express();
+app.listen(port,(res)=>{
+    app.get('/',(req, res)=>{res.status(200).send(`Server is running on port ${port}`)})
+    console.log(`Server is running on port ${port}`);
+});
