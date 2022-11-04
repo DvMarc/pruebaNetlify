e8ef1b8f5c9c4542d51e3122ae384ede06d6b139
DvMarc <romdmarc@espol.edu.ec>
2022-11-04 17:50:56 -0500

    7

--- src/app-root.js
+++ src/app-root.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const app = express();
-app.listen(port,(res)=>{
-  app.get('/',(req, res)=>{res.status(200).send(`Server is running on port ${port}`)})
-  console.log(`Server is running on port ${port}`);
-});
+import { customElement, property, LitElement, html, css } from 'lit-element';
+
+@customElement('app-root')
+export class AppRoot extends LitElement {
+  @property() message = 'Learn LitElement';
+  render() {
+    return {"hola": hola}
+  }
+}
